55f5a4fff30dfdc25d457ad0e395abe83bd1c8c0
BigMac6691 <gjm.q966@yahoo.com>
2024-09-25 21:46:29 -0400

    Created additional folders and added some stats to the files being merged.

--- game.js
+++ game.js
@@ -18,18 +18,22 @@ app.get('/', (req, res) =>
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/js/main.js', (req, res) => 
+app.get('/js/editor.js', (req, res) => 
 {
   logger.info('Sending merged JavaScript file...');
 
   const folders = 
   [
+    path.join(__dirname, "/js"), // put classes that other classes in the sub folders extend here for now
     path.join(__dirname, "/js/utils"),
     path.join(__dirname, "/js/map"),
-    path.join(__dirname, "/js/editor")
+    path.join(__dirname, "/js/editor"),
+    path.join(__dirname, "/js/editor/map"),
+    path.join(__dirname, "/js/editor/svg")
   ];
 
   let merge = "";
+  let totalSize = 0, count = 0, lines = 0, totalLines = 0;
 
   folders.forEach(folder => 
   {
@@ -40,18 +44,23 @@ app.get('/js/main.js', (req, res) =>
     {
       const fullPath = path.join(folder, file);
 
-      logger.info('Merging file...' + fullPath);
-
       if(fs.statSync(fullPath).isFile() && path.extname(fullPath) === ".js")
       {
         const content = fs.readFileSync(fullPath, "utf-8");
 
-        merge += content + "\n";
+        totalSize += content.length;
+        lines = content.split("\n").length;
+        totalLines += lines;
+        count++;
+
+        logger.info(`Merging ${file} count ${count}, size = ${content.length}, total size = ${totalSize}, lines = ${lines}, total lines ${totalLines}`);
+
+        merge += content + "\n\n";
       }        
     }); // loop for files in each folder
   }); // loop for source folders
 
-  logger.info(`Size of JavaScript...${merge.length}`);
+  logger.info(`Stats: file count ${count}, total size ${merge.length}`);
 
   res.setHeader('Content-Type', 'application/javascript');
   res.send(merge);
